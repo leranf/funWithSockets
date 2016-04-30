61a331b7517fdfa247a74395d5730b3ac3828c7f
Leran <leranf#gmail.com>
2016-04-29 17:50:05 -0700

    concatenates the winners of the current vote to entries

--- src/core.js
+++ src/core.js
@@ -1,11 +1,27 @@
 import {List, Map} from 'immutable';
 
+const getWinners = vote => {
+  if (!vote) {
+    return [];
+  }
+  const [a, b] = vote.get('pair');
+  const aVotes = vote.getIn(['tally', a], 0);
+  const bVotes = vote.getIn(['tally', b], 0);
+  if (aVotes > bVotes) {
+    return [a];
+  } else if (aVotes < bVotes) {
+    return [b];
+  } else {
+    return [a, b];
+  }
+}
+
 export setEntries = (state, entries) => {
   return state.set('entries', List(entries));
 };
 
 export next = state => {
-  const entries = state.get('entries');
+  const entries = state.get('entries').concat(getWinners(state.get('vote')));
   return state.merge({
     vote: Map({pair: entries.take(2)}),
     entries: entries.skip(2)
