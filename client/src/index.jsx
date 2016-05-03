1d240031fcf004f3d2afb665a36642d188a9fb70
Leran <leranf#gmail.com>
2016-05-03 14:59:34 -0700

    adds react router

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
+import {Route} from 'react-router';
+import App from './components/App';
 
 const pair = ['Trainspotting', '28 Days Later'];
+
+const routes = <Route component={App}>
+  <Route path="/" component={Voting} />
+</Route>;
 
 ReactDOM.render(
   <Voting pair={pair} winner="Trainspotting" />,
