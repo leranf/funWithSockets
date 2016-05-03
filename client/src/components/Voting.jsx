d4a33c4aac7679ab66aa1819046f4b0aad89e53d
Leran <leranf#gmail.com>
2016-05-03 14:22:30 -0700

    shows winner if there is one instead of trying to render more voting buttons

---
+++ client/src/components/Voting.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default React.createClass({
+  getPair: () => {
+    return this.props.pair || [];
+  },
+  isDisabled: function() {
+    return !!this.props.hasVoted;
+  },
+  hasVotedFor: entry => {
+    return this.props.hasVoted === entry;
+  },
+  render: () => (
+    <div className="voting">
+      {this.props.winner ?
+        <div ref="winner">Winner is {this.props.winner}!</div> :
+        {this.getPair().map(entry =>
+          <button key={entry} onClick={() => this.props.vote(entry)}
+            disabled={this.isDisabled()}>
+            <h1>{entry}</h1>
+            {this.hasVotedFor(entry) ? <div className="label">Voted</div> : null}
+          </button>
+        )}
+      }
+    </div>;
+  )
+});
